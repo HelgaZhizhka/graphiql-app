0fb03724519624a4c5abdfdbdbfeae630e25781a
wellder00 <wellder00@gmail.com>
2023-12-18 12:51:04 +0200

    fix: change handlePrettifyCode code

--- src/utils/handlePrettifyCode.ts
+++ src/utils/handlePrettifyCode.ts
@@ -1,9 +1,14 @@
+const isGraphQLOperation = (str: string) =>
+  ['mutation', 'query', 'subscription'].some((keyword) => str.includes(keyword));
+
+const indentWithSpaces = (deep: number, value: string) => `${'  '.repeat(deep) + value} `;
+const indentWithNewLine = (deep: number, value: string) => `${'  '.repeat(deep) + value}\n`;
+
 export const handlePrettifyCode = (
   code: string,
   setCode: React.Dispatch<React.SetStateAction<string>>
 ) => {
   const splitString = code.replace(/\s+/g, ' ').split(/(\{|})/);
-
   let deep = 0;
   let result = '';
 
@@ -16,27 +21,37 @@ export const handlePrettifyCode = (
     } else if (el.includes('}')) {
       deep = Math.max(deep - 1, 0);
       result += '  '.repeat(deep) + el.replace('}', '\n' + '  '.repeat(deep) + '}\n');
-    } else if (el.trim().split(' ').length >= 2) {
-      const moreWords = el.trim().split(' ');
-      moreWords.forEach((value, index) => {
-        if (moreWords.length - 1 === index) {
-          result += `${'  '.repeat(deep) + value} `;
+    } else if (el.trim().split(' ').length >= 2 || el.includes('(') || el.includes(')')) {
+      const tokenizedWords = el.trim().split(' ');
+      tokenizedWords.forEach((value, index) => {
+        const nextWord = tokenizedWords[index + 1];
+        if (tokenizedWords.length - 1 === index) {
+          result += indentWithSpaces(deep, value);
+        } else if (isGraphQLOperation(value)) {
+          nextWord && !isGraphQLOperation(nextWord)
+            ? (result += indentWithSpaces(deep, value))
+            : (result += indentWithNewLine(deep, value));
+        } else if (value.includes(',')) {
+          nextWord && !nextWord.includes(',')
+            ? (result += indentWithSpaces(deep, value))
+            : (result += indentWithNewLine(deep, value));
         } else if (value.includes(':')) {
-          const nextWord = moreWords[index + 1];
-          if (nextWord && !nextWord.includes(':')) {
-            result += `${'  '.repeat(deep) + value}`;
-          } else {
-            result += `${'  '.repeat(deep) + value}\n`;
-          }
+          nextWord && !nextWord.includes(':')
+            ? (result += indentWithSpaces(deep, value))
+            : (result += indentWithNewLine(deep, value));
         } else {
-          result += `${'  '.repeat(deep) + value}\n`;
+          result += indentWithNewLine(deep, value);
         }
       });
     } else {
       result += ' '.repeat(deep) + el;
     }
   }
-  result = result.replace(/}\n\s*\n/g, '}\n').replace(/:\s+/g, ': ');
+
+  result = result
+    .replace(/}\n\s*\n/g, '}\n')
+    .replace(/:\s+/g, ': ')
+    .replace(/(?<=\([^)]*)[ ]{2,}(?=[^()]*\))/g, ' ');
 
   setCode(result);
 };
